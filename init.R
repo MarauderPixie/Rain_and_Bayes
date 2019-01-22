b546bead0d63496759f7a6ccb051f3a20169080a
MarauderPixie <tobias.anton.85@gmail.com>
2019-01-22 11:21:13 +0100

    include probable essentials

--- init.R
+++ init.R
@@ -2,6 +2,11 @@ library(tidyverse)
 library(rstan)
 library(ggmcmc)
 
+### I'll probably need (one of) these:
+library(brms)   # especially this!
+                # promised jetpack: use the usual r-syntax for lm()
+# library(BayesFactor)
+
 setwd("./Bayesian_Cognitive_Modeling/ParameterEstimation/")
 theme_set(hrbrthemes::theme_ipsum_rc())
 
